f85e349bba9fb76495f310218f36086063a08897
Ferreira <pedroferreira.gomes03@gmail.com>
2026-02-25 20:44:21 -0300

    feat: finalizando estruturacao do projeto

---
+++ sistema-consultas/src/index.ts
@@ -0,0 +1,6 @@
+// import { Especialidade } from "./types/especialidade";
+// import { Paciente } from "./types/paciente";
+// import { StatusConsulta } from "./types/statusConsulta";
+// import { Medico } from "./interfaces/medico";
+// import { Consulta } from "./interfaces/consulta";
+
